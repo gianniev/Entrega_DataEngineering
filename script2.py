168291e05805bcbf334d1a45afd893a52117edc1
gianniev <74428622+gianniev@users.noreply.github.com>
2024-08-09 20:55:25 +0200

    Update script2.py

--- script2.py
+++ script2.py
@@ -5,12 +5,14 @@ import pandas as pd
 import os
 from sqlalchemy import create_engine 
 
+# Este scrpit extrae información de la base de datos REDSHIFT y la muestra en la terminal/visualstudio
+
 # Define la conexión a la base de datos
 conn_string = 'postgresql://gianni_ev93_coderhouse:r9NYpl19Zl@data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com:5439/data-engineer-database'
 engine = create_engine(conn_string)
 
 # Consulta SQL para leer datos de la tabla
-query = 'SELECT * FROM gianni_ev93_coderhouse.cryptocurrencies'
+query = 'SELECT * FROM gianni_ev93_coderhouse.coinmarketcap'
 
 # Leer datos desde la base de datos a un DataFrame
 df = pd.read_sql_query(query, con=engine)
